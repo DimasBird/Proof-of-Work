0344dba61c3505097db4c7b2515d5cadd5c74fd0
Dmitry Gutnikov <145686732+DimasBird@users.noreply.github.com>
2025-05-02 16:49:01 +0300

    Delete out directory

--- out/build/x64-Debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Program Files (x86)/git/Proof-of-Work/googletest/googlemock
-# Build directory: D:/Program Files (x86)/git/Proof-of-Work/out/build/x64-Debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- out/build/x64-Debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Program Files (x86)/git/Proof-of-Work/googletest/googletest
-# Build directory: D:/Program Files (x86)/git/Proof-of-Work/out/build/x64-Debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
