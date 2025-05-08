a0f36f47287f54589e92cccacadd26e5e3886adc
DimasBird <dagutnikov@edu.hse.ru>
2025-05-08 23:34:48 +0300

    schnorr curves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import struct
 from hash_function import streebog
+from pseudorandom_number_generator import PRNG
 
 
 # Example usage
@@ -7,3 +8,10 @@ if __name__ == "__main__":
     message = b"message digest"
     print("STREEBOG-512:", streebog(message, 512).hex())
     print("STREEBOG-256:", streebog(message, 256).hex())
+
+    Surname = "Medvedev"
+
+    generator = PRNG(Surname, overall_iterations=16)
+
+    for i in range(len(generator.numbers)):
+        print("{0}) {1}".format(i+1, generator.numbers[i].hex()))
--- pseudorandom_number_generator.py
+++ pseudorandom_number_generator.py
@@ -2,17 +2,19 @@ from hash_function import streebog
 
 class PRNG:
     def __init__(self, string, overall_iterations=1):
-        super(self).__init__()
-        self.h0 = streebog(PRNG.make_string_k_bits(string), hash_size=256)
+        super(PRNG, self).__init__()
+        self.h0 = streebog(PRNG.make_string_k_bytes(string, 64), hash_size=256)
         self.overall_iterations = overall_iterations
+        self.numbers = self.generate_numbers()
 
     @staticmethod
-    def make_string_k_bits(s, k=32) -> bytes:
-        if len(bytes(s) == 32):
+    def make_string_k_bytes(s, k=32) -> bytes:
+        s = s.encode('utf-8')
+        if len(bytes(s)) == 32:
             return bytes(s)
-        if len(bytes(s) > 32):
+        if len(bytes(s)) > 32:
             return bytes(s)[:32]
-        if len(bytes(s) < 32):
+        if len(bytes(s)) < 32:
             zeros = bytes([0 for i in range(k - len(s))])
             return bytes(s) + zeros
 
